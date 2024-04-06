38b448305db7e46d5f1bf0472f668143dd96caa5
asom1989 <asom.1989@yahoo.com>
2024-04-07 00:56:26 +0200

    hashed password edit login endpoint and  authMiddlware

--- middleware/authMiddlware.js
+++ middleware/authMiddlware.js
@@ -1,16 +1,23 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 export const authenticateJWT = (req, res, next) => {
-    const token = req.headers.authorization;
-  
-    if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-  
-    jwt.verify(token, secretKey, (err, decoded) => {
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
-        return res.status(403).json({ message: 'Forbidden' });
+        return res.status(403).json({ message: "Forbidden" });
       }
       req.user = decoded;
+      console.log(decoded);
       next();
     });
-  };
+  } catch (error) {
+    return res.status(401).json({ error: error.message });
+  }
+};
